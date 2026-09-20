274f84d5357e36a1f9d823def64fb35f9ef3c60b
steph45acke-hue <steph45.ac.ke@gmail.com>
2026-09-20 15:30:46 +0300

    Finalize Part 2 sales forecasting model, R-squared metrics, and interactive HTML dashboards

--- part2_forecasting.py
+++ part2_forecasting.py
@@ -29,3 +29,44 @@ print("Daily sales data successfully loaded for modeling!\n")
 
 #data preprocessing and numerical indexing
 df_daily['transaction_date'] = pd.to_datetime(df_daily['transaction_date'])
+df_daily['day_index'] = np.arange(len(df_daily))
+
+
+#fitting the linear regression model(OLS)
+x= df_daily['day_index']
+y = df_daily['daily_revenue']
+
+slope,intercept = np.polyfit(x,y,1)
+df_daily['trend_line'] = (slope * x) + intercept
+print(f"calculated sales trend slope:{slope:.2f} KES per day")
+
+#evaluate the accuracy of the model
+
+y_mean = np.mean(y)
+ss_total = np.sum((y-y_mean) ** 2)
+ss_residual = np.sum((y-df_daily['trend_line']) ** 2)
+r_squared = 1 - (ss_residual / ss_total)
+
+print(f"Model r-squared(R2):{r_squared:.4f}")
+
+
+#plotting and exporting with plotly
+fig = px.scatter(
+    df_daily,
+    x='transaction_date',
+    y='daily_revenue',
+    title='kilele retail - daily sales and revenue trend model(R2= {r_squared:.2f})',
+    labels = {'transaction date':'Transaction Date','daily_revenue':'Daily Revenue(KES)'}
+)
+
+fig.add_scatter(
+    x=df_daily['transaction_date'],
+    y = df_daily['trend_line'],
+    mode='lines',
+    name= 'Linear Trend Projection',
+    line = dict(color='red',width = 3,dash='dash')
+)
+
+fig.write_html("sales_forecast_model.html")
+print("Forecast model chart successfully saved as 'sales_forecast_model.html'!")
+fig.show()
